6fed88f6ca38012d783fb8ad170a4ed4db3bbfe6
Daniel Siviter <dansiviter@gmail.com>
2021-04-04 12:28:43 +0100

    Fixing GraalVM

---
+++ src/main/java/uk/dansiviter/helidon/ServiceContextDecorator.java
@@ -0,0 +1,7 @@
+package uk.dansiviter.helidon;
+
+public class ServiceContextDecorator extends uk.dansiviter.gcp.log.ServiceContextDecorator {
+	public ServiceContextDecorator() {
+		super(ServiceContextDecorator.class);
+	}
+}
