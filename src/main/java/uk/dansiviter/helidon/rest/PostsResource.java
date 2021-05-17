fcf5beba702ab7138b06db1385ac9a1de6779f98
Daniel Siviter <dansiviter@gmail.com>
2021-05-17 22:22:46 +0100

    More upgrades

--- src/main/java/uk/dansiviter/helidon/rest/PostsResource.java
+++ src/main/java/uk/dansiviter/helidon/rest/PostsResource.java
@@ -2,8 +2,8 @@ package uk.dansiviter.helidon.rest;
 
 import java.util.List;
 import java.util.Optional;
+import java.util.concurrent.CompletionStage;
 
-import javax.annotation.Nonnull;
 import javax.enterprise.context.RequestScoped;
 import javax.inject.Inject;
 import javax.ws.rs.GET;
@@ -26,15 +26,13 @@ public class PostsResource {
 	private JsonPlaceholderService service;
 
 	@GET
-	public List<Post> posts() {
-		// oracle/helidon#2910
-		return this.service.posts().toCompletableFuture().join();
+	public CompletionStage<List<Post>> posts() {
+		return this.service.posts();
 	}
 
 	@GET
-	@Path("{id}") // /oracle/helidon#2913
-	public Optional<Post> post(@PathParam("id") @Nonnull Integer id) {
-		// oracle/helidon#2910
-		return this.service.post(id).toCompletableFuture().join();
+	@Path("{id}")
+	public CompletionStage<Optional<Post>> post(@PathParam("id") int id) {
+		return this.service.post(id);
 	}
 }
